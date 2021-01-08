57a0fe21a4c702f9aa77a1ea668bd54dfeecaf9b
Rafael Lozano <rafa.lozano.rolon@gmail.com>
2021-01-08 09:51:25 -0600

    Login page with AWS

--- App.js
+++ App.js
@@ -5,20 +5,17 @@ import Amplify from 'aws-amplify'
 import config from './aws-exports'
 Amplify.configure(config)
 
-export default function App() {
+import { withAuthenticator } from 'aws-amplify-react-native'
+
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>Testing APP</Text>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default withAuthenticator(App)
+
+
